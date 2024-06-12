ecdb1012246a50f2cbe4ffd6fdd4da03262d548c
Nguyễn Viết Minh <92623836+Min-KiD@users.noreply.github.com>
2024-06-11 22:01:37 -0700

    Update yolo_anchor_assigner.py

--- models/assigner/yolo_anchor_assigner.py
+++ models/assigner/yolo_anchor_assigner.py
@@ -693,8 +693,8 @@ class YOLOAnchorAssigner(nn.Module):
             # a = t[:, 6].long()  # anchor indices
             a = t[:, 7].long()  # anchor indices
             score = t[:,6].T
-            indices.append((b, a, gj.clamp_(0, gain[3] - 1), gi.clamp_(0, gain[2] - 1)))  # image, anchor, grid indices
-            tbox.append(torch.cat((gxy - gij, gwh), 1))  # box
+            indices.append((b, a, gj.clamp_(0, int(gain[3] - 1)), gi.clamp_(0, int(gain[2] - 1))))  # image, anchor, grid indices
+            tbox.append(torch.cat((gxy - gij.float(), gwh), 1))  # box
             anch.append(anchors[a])  # anchors
             tcls.append(c)  # class
             tscore.append(score)
